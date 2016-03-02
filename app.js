796c1e5c6c9db8f1ac8668fbe02827ac56664033
Ben <bdspen@gmail.com>
2016-03-01 23:14:56 -0800

    use strict and partial fix for new user appearance

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var app = angular.module("WM", [
     'ui.router',
     'firebase',
@@ -14,96 +15,91 @@ var app = angular.module("WM", [
 
 app.constant('fbUrl', "https://worldmessage.firebaseio.com");
 
-app.run(function($state, $rootScope) { //allows for statechange errors to be shown in console
+app.run(['$state', '$rootScope', function($state, $rootScope) { //allows for statechange errors to be shown in console
     $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
         console.error("Something went wrong!", error);
         console.error("$stateChangeError: ", toState, error);
     });
-    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
         $rootScope.containerClasses = toState.containerClasses;
     });
-    $rootScope.authOnce = true;
     $state.go('home'); //go home on start
     $rootScope.$state = $state;
-});
+}]);
 
-app.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
-            $stateProvider.state('home', {
-                    url: '/',
-                    controller: 'HomeCtrl',
-                    templateUrl: 'views/home.html',
-                    containerClasses: ["col-md-8", "col-md-4"],
-                    resolve: {
-                        resources: function(AuthService, UserService, MessageService, $rootScope) {
-                            if (!AuthService.authData && $rootScope.authOnce == true) {
-                                AuthService.anon();
-                                $rootScope.authOnce = false;
-                            }
-                            if ($rootScope.selectedUser) {
-                                MessageService.removePing($rootScope.user.uid, $rootScope.selectedUser.uid);
-                            }
-
-                                $rootScope.users = UserService();
-                                var resources = {
-                                    AuthService: AuthService,
-                                    MessageService: MessageService
-                                }
-                                return resources;
-                            },
-                        }
-                    }).state('chat', {
-                    url: '/chat/:selectedUid/:uid',
-                    controller: 'MessageCtrl',
-                    templateUrl: 'views/home.html',
-                    containerClasses: ["col-md-5", "col-md-7"],
-                    resolve: {
-                        resources: function(MessageService, $rootScope, $stateParams, $firebaseObject, fbUrl) {
-                            var selectedUserRef = new Firebase(fbUrl).child('users').child($stateParams.selectedUid);
-                            var resources = {
-                                uid: $stateParams.uid,
-                                selectedUser: {},
-                                MessageService: MessageService
-                            }
-                            $firebaseObject(selectedUserRef).$loaded().then(function(data) {
-                                resources.selectedUser = data;
-                            });
-
-                            MessageService.createPing($stateParams.uid, $stateParams.selectedUid); //send ping to trigger.once()function in homectrl
-                            MessageService.get($stateParams.uid, $stateParams.selectedUid).then(function(data) {
-                                $rootScope.messages = data;
-                            });
-
-
-                            return resources;
-                        },
-                    }
-                }).state('chatrequest', {
-                    url: '/chatrequest/:selectedUid/:uid',
-                    controller: 'MessageCtrl',
-                    templateUrl: 'views/home.html',
-                    containerClasses: ["col-md-5", "col-md-7"],
-                    resolve: {
-                        resources: function(MessageService, $rootScope, $stateParams, $firebaseObject, fbUrl) {
-
-                            var selectedUserRef = new Firebase(fbUrl).child('users').child($stateParams.selectedUid);
-                            var resources = {
-                                uid: $stateParams.uid,
-                                selectedUser: {},
-                                MessageService: MessageService
-                            }
-                            $firebaseObject(selectedUserRef).$loaded().then(function(data) {
-                                resources.selectedUser = data;
-                            });
-                            MessageService.createPing($stateParams.uid, $stateParams.selectedUid); //send ping to trigger.once()function in homectrl
-                            MessageService.get($stateParams.uid, $stateParams.selectedUid).then(function(data) {
-                                $rootScope.messages = data;
-                            });
-
-                            return resources;
-                        },
-                    }
-                }); $urlRouterProvider.otherwise("/"); $locationProvider.html5Mode({
-                    enabled: true,
-                    requireBase: false
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        controller: 'HomeCtrl',
+        templateUrl: 'views/home.html',
+        containerClasses: ["col-md-8", "col-md-4"],
+        resolve: {
+            resources: function(AuthService, UserService, MessageService, $rootScope) {
+                if(!AuthService.authData){
+                    AuthService.anon();
+                }
+                $rootScope.users = UserService();
+                var resources = {
+                    AuthService: AuthService,
+                    MessageService: MessageService
+                }
+                return resources;
+            },
+        }
+    }).state('chat', {
+        url: '/chat/:selectedUid/:uid',
+        controller: 'MessageCtrl',
+        templateUrl: 'views/home.html',
+        containerClasses: ["col-md-5", "col-md-7"],
+        resolve: {
+            resources: function(MessageService, $rootScope, $stateParams, $firebaseObject, fbUrl) {
+                var selectedUserRef = new Firebase(fbUrl).child('users').child($stateParams.selectedUid);
+                var resources = {
+                    uid: $stateParams.uid,
+                    selectedUser: {},
+                    MessageService: MessageService
+                }
+                $firebaseObject(selectedUserRef).$loaded().then(function(data){
+                    resources.selectedUser = data;
                 });
-            });
+
+                MessageService.createPing($stateParams.uid, $stateParams.selectedUid); //send ping to trigger.once()function in homectrl
+                MessageService.get($stateParams.uid, $stateParams.selectedUid).then(function(data) {
+                    $rootScope.messages = data;
+                });
+
+
+                return resources;
+            },
+        }
+    }).state('chatrequest', {
+        url: '/chatrequest/:selectedUid/:uid',
+        controller: 'MessageCtrl',
+        templateUrl: 'views/home.html',
+        containerClasses: ["col-md-5", "col-md-7"],
+        resolve: {
+            resources: function(MessageService, $rootScope, $stateParams, $firebaseObject, fbUrl) {
+
+                var selectedUserRef = new Firebase(fbUrl).child('users').child($stateParams.selectedUid);
+                var resources = {
+                    uid: $stateParams.uid,
+                    selectedUser: {},
+                    MessageService: MessageService
+                }
+                $firebaseObject(selectedUserRef).$loaded().then(function(data){
+                    resources.selectedUser = data;
+                });
+                MessageService.get($stateParams.uid, $stateParams.selectedUid).then(function(data) {
+                    $rootScope.messages = data;
+                });
+
+                return resources;
+            },
+        }
+    });
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode({
+        enabled:true,
+        requireBase: false
+    });
+}]);
