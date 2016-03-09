d140427c8ceb7499f7e816b11f0e5e2c63f85506
Ben <bdspen@gmail.com>
2016-03-08 21:56:57 -0800

    Update 2016-03-09T05:56:53.490Z

--- services/UserService.js
+++
@@ -1,6 +0,0 @@
-angular.module('WM').factory('UserService', ['$firebaseArray', 'FirebaseService', function($firebaseArray, FirebaseService) {
-    return function() {
-        var usersRef = FirebaseService.child('users');
-        return $firebaseArray(usersRef);
-    }
-}]);
